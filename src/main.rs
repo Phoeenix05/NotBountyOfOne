8651aaafa366abed9c0b7e74b5f5c62d16c4e43f
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-10-19 08:49:55 +0300

    feat: spawn a projectile on click

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,54 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::PresentMode;
+
+// #[derive(Bundle)]
+// struct ProjectileBundle<M: Asset> {
+//     projectile: Projectile<M>,
+// }
+
+#[derive(Component)]
+struct Projectile {
+    velocity: Vec2,
+    transform: Transform,
+}
+
+fn spawn_projectile(
+    mut commands: Commands,
+    buttons: Res<Input<MouseButton>>,
+    window_query: Query<&Window>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if !buttons.just_pressed(MouseButton::Left) {
+        return;
+    }
+
+    let (mouse_x, mouse_y) = match window_query.get_single() {
+        Ok(window) => window.cursor_position().unwrap().into(),
+        Err(_) => return,
+    };
+
+    let transform = Transform::from_xyz(mouse_x, mouse_y, 0.0).with_scale(Vec3::splat(128.));
+    dbg!(transform, (mouse_x, mouse_y));
+
+    commands.spawn((
+        Projectile {
+            velocity: (1.0, 0.0).into(),
+            transform,
+        },
+        MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
+            transform,
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            ..default()
+        },
+    ));
+}
 
 fn main() {
     let mut app = App::new();
-    
+
     app.add_plugins(DefaultPlugins.set(WindowPlugin {
         primary_window: Some(Window {
             title: "Not... Bounty of One".into(),
@@ -18,6 +63,10 @@ fn main() {
 
     app.add_systems(Startup, |mut commands: Commands| {
         commands.spawn(Camera2dBundle::default());
+    });
+    app.add_systems(Update, spawn_projectile);
+    app.add_systems(Update, |mut query: Query<&Transform, With<Projectile>>| {
+        println!("{:#?}", query)
     });
 
     app.run();
